3d76fb6d0778d8722d4763627352705ad2639e24
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-03-04 12:45:21 +0530

    Add PE and val_acc

--- model.py
+++ model.py
@@ -1,5 +1,31 @@
-from typing import Any
+import math
 from data import *
+
+class PositionalEncoding(nn.Module):
+    """
+    Implements sinusoidal positional encoding.
+    NB: This adds the PE vector to the embedding. It does NOT return the PE vector.
+    Taken from https://pytorch.org/tutorials/beginner/transformer_tutorial.html,
+    modified to be `batch_first=True`.
+    """
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
+        """
+        x = x + self.pe.repeat(x.size(0), 1, 1)
+        return self.dropout(x)
 
 class PEGParser(pl.LightningModule):
     def __init__(self, vocab_size,
@@ -7,11 +33,14 @@ class PEGParser(pl.LightningModule):
                        d_model=32,
                        nhead=2,
                        num_layers=3,
+                       max_len=20,
                        lr=0.1):
         super().__init__()
         self.lr = lr
+        self.criterion = nn.CrossEntropyLoss()
 
         self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+        self.pos_encoder = PositionalEncoding(d_model=d_model, max_len=max_len)
         layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=d_model, batch_first=True)
         self.transformer = nn.TransformerEncoder(layer, num_layers=num_layers)
         self.head = nn.Linear(in_features=d_model, out_features=out_possibilities)
@@ -20,37 +49,38 @@ class PEGParser(pl.LightningModule):
             # [bz, seq]
             self.embedding,
             # [bz, seq, d_model]
+            self.pos_encoder,
+            # [bz, seq, d_model]
             self.transformer,
             # [bz, seq, d_model]
             self.head
-            # [bz, seq, num_possibilities]
+            # [bz, seq, out_possibilities]
         )
 
     def forward(self, strings):
-        return self.model(strings).flatten(0, 1)
+        return self.model(strings)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
 
     def training_step(self, batch, batch_idx):
         strings, results = batch
-        logits = self.forward(strings)
-        loss = nn.CrossEntropyLoss()(logits, results.flatten(0, 1))
+        logits = self(strings)
+        loss = self.criterion(logits.flatten(0, 1), results.flatten(0, 1))
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         strings, results = batch
-        logits = self.forward(strings)
-        loss = nn.CrossEntropyLoss()(logits, results.flatten(0, 1))
-        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+        logits = self(strings)
+        preds = torch.argmax(logits, dim=-1)
+        accuracy = (preds == results).sum() / strings.numel()
+        self.log("val_acc", accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return accuracy
 
     def test_step(self, batch, batch_idx):
         strings, results = batch
-        bz, seq = strings.shape
-
         logits = self.forward(strings)
         preds = torch.argmax(logits, dim=-1)
-        accuracy = (preds == results.flatten(0,1)).sum() / (bz * seq)
+        accuracy = (preds == results).sum() / strings.numel()
         return accuracy
--- run.py
+++ run.py
@@ -18,23 +18,55 @@ NUM_RESULTS = {
     'dyck3': 2
 }
 
+SEQ_LENGTHS = {
+    'star': 20,
+    'brack': 20,
+    'expr': 21,
+    'triple': 20,
+    'dyck1': 20,
+    'dyck3': 20
+}
+
 BATCH_SIZE = 1024
 LR = 5e-4
 LANGUAGE = 'star'
 
 VOCAB_SIZE = VOCAB_SIZES[LANGUAGE]
 OUT_POSSIBILITIES = NUM_RESULTS[LANGUAGE]
+MAX_LEN = SEQ_LENGTHS[LANGUAGE]
 
 dataModule = PEGDataModule(LANGUAGE, BATCH_SIZE)
-model = PEGParser(VOCAB_SIZE, OUT_POSSIBILITIES)
+model = PEGParser(vocab_size=VOCAB_SIZE, out_possibilities=OUT_POSSIBILITIES, max_len=MAX_LEN)
 
-earlyStopper = EarlyStopping(monitor='val_loss', check_on_train_epoch_end=False)
+earlyStopper = EarlyStopping(monitor='val_acc', mode='max', check_on_train_epoch_end=False)
 lrFinder = LearningRateFinder()
-saveCkpt = ModelCheckpoint(dirpath='models/', filename='{epoch}-{val_loss:.2f}', monitor='val_loss', save_on_train_epoch_end=False)
+saveCkpt = ModelCheckpoint(dirpath='models/', filename='{epoch}-{val_acc:.3f}', monitor='val_acc', mode='max', save_on_train_epoch_end=False)
 trainer = pl.Trainer(callbacks=[lrFinder, saveCkpt, earlyStopper], max_epochs=-1)
 
+trainer.validate(model, dataloaders=dataModule.val_dataloader())
 trainer.fit(model,
             train_dataloaders=dataModule.train_dataloader(),
             val_dataloaders=dataModule.val_dataloader())
 trainer.test(model,
              dataloaders=dataModule.test_dataloader())
+
+"""
+Classification Report
+"""
+#inputs = dataModule.test_dataset.input # [lines, L]
+#outputs = dataModule.test_dataset.output # [lines, L]
+#lines, L = outputs.shape
+#preds = torch.zeros((lines, L))
+#for i in range(0, lines, 1024):
+#    if (i+1024 >= lines):
+#        logits = model(inputs[i:lines, :])
+#        preds[i:lines, :] = torch.argmax(logits, dim=-1)
+#    else:
+#        logits = model(inputs[i:i+1024, :]) # [1024, L, n]
+#        preds[i:i+1024, :] = torch.argmax(logits, dim=-1)
+#
+#from sklearn.metrics import classification_report
+#
+#print((outputs == 0).sum(), (outputs == 1).sum())
+#print((preds == 0).sum(), (preds == 1).sum())
+#classification_report(outputs.flatten(), preds.flatten())
